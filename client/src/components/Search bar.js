3ed943e5e6a1c06a3d9ddba7c71ff851167ac119
Wambui Ndungu <wambuindungu4967@gmail.com>
2025-05-20 01:57:08 +0400

    adds a signup section

---
+++ client/src/components/Search bar.js
@@ -0,0 +1,20 @@
+/*import React, { useState } from "react"
+function SearchBar({onSearch}){
+  const [searchData, setSearchData] = useState ({})
+};
+return(
+  <div>
+<h2> search</h2>
+<form >
+<input
+type="text"
+placeholder="search..."
+/> 
+</form>
+
+
+  </div>
+)
+  
+
+export default SearchBar;*/
