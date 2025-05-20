6b5e61555e1e84c8b5788169d8e496de9c1affe0
Wambui Ndungu <wambuindungu4967@gmail.com>
2025-05-21 02:19:51 +0400

    creates a user and search cache table

--- server/models.py
+++ server/models.py
@@ -1,2 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import Datetime 
+from datetime import datetime 
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.dialects.postgresql import JSON
+
+db =SQLAlchemy()
+
+class User(db.Model,SerializerMixin):
+    __tablename__='users'
+
+    id =db.Column(db.Integer, primary_key=True)
+    firstname =db.Column(db.String, nullable=False)
+    surname =db.Column(db.String, nullable=False)
+    email =db.Column(db.String(234), unique=True, nullable=False)
+    password_has =db.Column(db.String(128), nullable=False)
+    
+
+
+       
+
+class SearchCache(db.Model, SerializerMixin):
+    __tablename__='search_cache'
+
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, default = datetime.utcnow)
+    search_term = db.Column(db.String, nullable=False)
+    results = db.Column(JSON, nullable=False)
+
+
