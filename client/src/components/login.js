801417c629d6fc2c9535b04fe34db79433e2f079
Wambui Ndungu <wambuindungu4967@gmail.com>
2025-05-22 09:15:01 +0400

    implements handle submit

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,8 +1,18 @@
-import React, {useState}from "react";
+import React, {useState, useEffect}from "react";
+import {link, useNavigate} from"react-router-dom";
 
 function Login(){
-  const [loginData, setLoginData] = useState({email:"", password:""})
+  const [loginData, setLoginData] = useState({
+    email:"", 
+    password:""
+  });
+  
 
+  const[error, setError]= useState("");
+  const navigate = useNavigate();
+  const API_BASE_URL = process.env.REACT_APP_API_URL;
+
+  
   const handleChange=(e)=>{
     setLoginData({
       ...loginData,
@@ -11,9 +21,42 @@ function Login(){
   };
     
 
-const handleSubmit=(e)=>{
-  e.preventDefault();
-};
+
+  const handleSubmit= async (e)=>{
+    e.preventDefault();
+    setError();
+  
+    try {
+        const response = await fetch(`${API_BASE_URL}/login`,{
+          method:"POST",
+          headers: {
+            'Content_Type':'application.json'
+             },
+             body:JSON.stringify({
+              email:loginData.email,
+               password:loginData.password
+              })
+        });
+        const data = await response.json();
+
+        if(!response.ok){
+          throw new Error(data.message ||"login failed")
+        }
+
+        localStorage.setItem("token", data.access_token);
+        localStorage.setItem("username", data.name || "User");
+    
+  
+      }catch (error) {
+        console.error("Error logging in:", error.message);
+      }
+    };
+
+
+
+
+
+
 return(
   <div>
     <h2> Login</h2>
