0af6d0d5012bae097bbf70c39f7d7822cb00d487
Wambui Ndungu <wambuindungu4967@gmail.com>
2025-06-23 04:39:32 +0400

    comments out in .emv file

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const BASE_URL = process.env.NODE_ENV === 'production'
-  ?" https://sw-search-api.onrender.com"
+  ?"https://sw-search-api.onrender.com"
   : "http://localhost:3006";
 
 export default BASE_URL;
